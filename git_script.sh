870ff02faccc8660bba341852cc1d8ad305a1cee
abirchLBG <alexander.birch@e.lloydsbanking.com>
2023-06-09 02:14:55 +0100

    "status check"

--- git_script.sh
+++ git_script.sh
@@ -3,11 +3,16 @@
 NC=$'\033[0m'
 BGreen=$'\033[1;32m'
 BRed=$'\033[1;31m'
+BOLD=$(tput bold)
+NOT_BOLD=$(tput sgr0)
 prefix="[${BGreen}Git-Script${NC}]"
 
 
 input_args=""
 branch_name=""
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+git_status=$(git status)
+pull_status="false"
 
 # Checks if commit msg is empty
 check_empty_msg() {
@@ -25,6 +30,7 @@ check_empty_msg() {
 
 # (Not actually main lmao)
 main() {
+    check_status
     main_args_list=$@
     if [ $# -eq 0 ]; then
         input_args=$(check_empty_msg "c" "$*") 
@@ -64,9 +70,8 @@ main() {
 
 # (main) Function to check if the branch is master/main
 check_branch() {
-    branch_name=$(git rev-parse --abbrev-ref HEAD)
     if [[ "$branch_name" = "master" || "$branch_name" = "main" ]]; then
-        read -r "?$prefix Current branch is '${BRed}$branch_name${NC}'. Continue (Y/n)? " choice
+        read -r "?$prefix Current branch is '${BRed}$branch_name${NC}'. Continue ${BOLD}(Y/n)${NOT_BOLD}? " choice
         case $choice in
             [yY]) main $@ ;;
             [nN]) echo "$prefix Git script terminated.";;
@@ -79,13 +84,35 @@ check_branch() {
 custom_msg() {
     # echo "git add -A"
     # echo "git commit -m \"$*\""
+    # if [ "$pull_status" = "true" ]; then
+    #     echo "git pull"
+    # fi
     # echo "git push"
     git add -A
     git commit -m \"$*\"
+    if [ "$pull_status" = "true" ]; then
+        git pull
+    fi
     git push
     echo "$prefix Git script finished successfully!"
     exit 1
 }
+
+check_status() {
+    if [[ $git_status =~ "Your branch is behind" ]]; then
+        read -r "?$prefix Branch is behind '${BGreen}$branch_name${NC}'. Git pull ${BOLD}(Y/n)${NOT_BOLD}? " pull_choice
+        case $pull_choice in
+            [yY]) pull_status="true" ;;
+            [nN]) echo "$prefix Git script terminated."
+                    exit 1;;
+            *) check_status ;;
+        esac
+    elif [[ $git_status =~ "nothing to commit" ]]; then
+        echo "$prefix No changes made. Exiting."
+        exit 1
+    fi
+}
+
 
 # main() func call
 check_branch $@
--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
 #!/bin/zsh
 
+branch_name=master
+NC=$'\033[0m'
+BGreen=$'\033[1;32m'
+BRed=$'\033[1;31m'
+prefix="[${BGreen}Git-Script${NC}]"
+
 check_status() {
-    git_status=$(git status)
-    echo "st $git_status"
     if [[ $git_status =~ "nothing to commit" ]]; then
-        echo "No changes made"
-    else
-        echo "changes made"
+        echo "$prefix No changes made. Exiting."
+        exit 1
+    elif [[ $git_status =~ "Your branch is behind" ]]; then
+        read -r "?$prefix Branch is behind '${GReen}$branch_name${NC}'. Git pull (Y/n)? " pull_choice
+        case $pull_choice in
+            [yY]) pull_status="true" ;;
+            [nN]) echo "$prefix Git script terminated.";;
+            *) check_status ;;
+        esac
     fi
 }
 
