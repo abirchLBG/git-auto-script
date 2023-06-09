b00a5fc35b07420f2cc70bb5fca277ef18edbfeb
abirchLBG <alexander.birch@e.lloydsbanking.com>
2023-06-09 01:43:11 +0100

    "upd"

--- test.sh
+++ test.sh
@@ -3,6 +3,11 @@
 check_status() {
     git_status=$(git status)
     echo "st $git_status"
+    if [[ "nothing to commit" = *"$git_status"* ]]; then
+        echo "No changes made"
+    else
+        echo "changes made"
+    fi
 }
 
 check_status
