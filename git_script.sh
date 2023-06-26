d3d71f477bfec10f289cefd9ea7c0d27de833c6e
abirchLBG <alexander.birch@e.lloydsbanking.com>
2023-06-26 18:11:38 +0100

    feat: main blue instead of green

--- git_script.sh
+++ git_script.sh
@@ -4,12 +4,14 @@
 NC=$'\033[0m'
 BGreen=$'\033[1;32m'
 BRed=$'\033[1;31m'
-BYellow=$'\033[1;33m' 
+BYellow=$'\033[1;33m'
+BBlue=$'\033[1;34m'
 BOLD=$(tput bold)
 NOT_BOLD=$(tput sgr0)
 prefix="[${BGreen}Git-Script${NC}]"
 red_prefix="[${BRed}Git-Script${NC}]"
 yellow_prefix="[${BYellow}Git-Script${NC}]"
+blue_prefix="[${BBlue}Git-Script${NC}]"
 
 
 input_args=""
@@ -105,15 +107,15 @@ custom_msg() {
     git commit -m $*
     echo "$msg" > "$script_loc/last_commit_msg.txt"
     if [ "$pull_status" = "true" ]; then
-        echo "$prefix Executing git pull."
+        echo "$blue_prefix Executing git pull."
         git pull
     fi
     git push --quiet
     status_check=$(git status)
     if [[ "$status_check" =~ "up to date" ]]; then
-        echo "$prefix Current branch is now up to date with remote."
+        echo "$blue_prefix Current branch is now up to date with remote."
     else
-        echo "$prefix Git push finished, but branch is not up to date with remote. Pulling."
+        echo "$blue_prefix Git push finished, but branch is not up to date with remote. Pulling."
         git pull
     fi
     echo "$prefix ${BOLD}Git script finished successfully!${NOT_BOLD}"
@@ -124,7 +126,7 @@ custom_msg() {
 check_status() {
     # echo "INFO: check_status() called"
     if [[ $git_status =~ "Your branch is behind" ]]; then
-        read -r "?$prefix Branch is behind '${BGreen}$branch_name${NC}'. Git pull ${BOLD}(Y/n)${NOT_BOLD}? " pull_choice
+        read -r "?$yellow_prefix Branch is behind '${BGreen}$branch_name${NC}'. Git pull ${BOLD}(Y/n)${NOT_BOLD}? " pull_choice
         case $pull_choice in
             [yY]) pull_status="true" ;;
             [nN]) echo "$prefix Git script terminated."
@@ -138,7 +140,7 @@ check_status() {
 }
 
 
-echo "$prefix ${BOLD}Starting Git-Script.${NOT_BOLD}"
+echo "$blue_prefix ${BOLD}Starting Git-Script.${NOT_BOLD}"
 # main() func call
 check_branch $@
 
